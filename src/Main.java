da4086f508f9326f8126306b6611d82df61a9cc4
Aymen Hajry <aymanehajry4@gmail.com>
2025-04-22 18:16:23 +0100

    Ajout de la fonctionnalité de modification maison

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-       //new UserDashboard();
 
         new Home();
+
+       //new UserDashboard();
+
+
         //new browseHouses(1);
         //new Inscrire();
-
         //new LoginUI();
         //new AdminDashboardSwing();
 
